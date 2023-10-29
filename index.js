ae3d6eb57e8e414f642f9b002275bbd3f65dbb89
Moh Bareche <32602327+MohBareche@users.noreply.github.com>
2023-10-29 05:40:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:["https://moh-app.netlify.app/"],
+    origin:["https://moh-app.netlify.app"],
     methods:["POST","GET"],
     credentials:true
 }));
